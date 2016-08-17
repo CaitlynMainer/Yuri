dc5eee11d3f58752b58e3a4dddf216723d5d2bd7
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2016-08-17 10:50:12 -0500

    fake useragent for Avatar Requests

--- src/main/java/net/dv8tion/discord/commands/SetAvatar.java
+++ src/main/java/net/dv8tion/discord/commands/SetAvatar.java
@@ -27,8 +27,12 @@ import net.dv8tion.jda.events.message.priv.GenericPrivateMessageEvent;
 import net.dv8tion.jda.managers.AccountManager;
 import net.dv8tion.jda.utils.AvatarUtil;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -49,8 +53,14 @@ public class SetAvatar extends Command
                 .appendString("New avatar set!")
                 .build());
         try {
-            URL imageURL = new URL(args[1]);
-            accountManager.setAvatar(AvatarUtil.getAvatar(imageURL.openStream()));
+
+            URLConnection connection = new URL(args[1]).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            connection.connect();
+
+            //BufferedReader r  = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
+
+            accountManager.setAvatar(AvatarUtil.getAvatar(connection.getInputStream()));
             accountManager.update();
         } catch (IOException e1) {
             e1.printStackTrace();
