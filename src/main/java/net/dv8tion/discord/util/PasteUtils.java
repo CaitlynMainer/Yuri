b78149ffb289c1fba8c00399d78450290e939b60
Caitlyn <Michiyo.Ravencroft@gmail.com>
2020-01-10 08:18:29 -0800

    Update PasteUtils.java

--- src/main/java/net/dv8tion/discord/util/PasteUtils.java
+++ src/main/java/net/dv8tion/discord/util/PasteUtils.java
@@ -64,7 +64,7 @@ public class PasteUtils {
 
             //Get Response
             BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            return pasteURL.replace("http", "https") + new JSONObject(rd.readLine()).getString("key");
+            return new JSONObject(rd.readLine()).getString("key");
 
         } catch (IOException | JSONException e) {
             e.printStackTrace();
